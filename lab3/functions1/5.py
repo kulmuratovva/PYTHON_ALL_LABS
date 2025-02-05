b5508d0e38d289cffff59302e675c054c3326462
Anel Kulmuratova <anelkulmuratovaaa@gmail.com>
2025-02-05 05:37:34 +0500

    Update 5.py

--- lab3/functions1/5.py
+++ lab3/functions1/5.py
@@ -1,9 +1,9 @@
-def permute(s, answer=""):
-    if len(s) == 0:
-        print(answer)
-        return
-    for i in range(len(s)):
-        permute(s[:i] + s[i+1:], answer + s[i])
+import itertools
 
-w = input("Enter the word: ")
+def permute(word):
+    permut = itertools.permutations(word)
+    for per in permut:
+        print("".join(per))
+        
+w = input()
 permute(w)
