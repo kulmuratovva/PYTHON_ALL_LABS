1b397bbed7447e9832ee779b23e3ebd15276f0b9
Anel Kulmuratova <anelkulmuratovaaa@gmail.com>
2025-02-12 10:05:50 +0500

    Add files via upload

---
+++ lab4/math/1.py
@@ -0,0 +1,9 @@
+# Write a Python program to convert degree to radian.
+
+import math
+
+d = int(input())
+
+rad = d * (math.pi / 180)
+
+print(rad)
---
+++ lab4/math/4.py
@@ -0,0 +1,8 @@
+# Write a Python program to calculate the area of a parallelogram.
+
+b = float(input("Length of base: "))
+
+h = float(input("Height: "))
+
+area = b * h
+print(area)
