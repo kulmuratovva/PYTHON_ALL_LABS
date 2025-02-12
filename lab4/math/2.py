fb6063e81c736ab6fd486fb06279deed4fcb07a9
Anel Kulmuratova <anelkulmuratovaaa@gmail.com>
2025-02-12 10:06:41 +0500

    Add files via upload

---
+++ lab4/math/2.py
@@ -0,0 +1,9 @@
+# Write a Python program to calculate the area of a trapezoid.
+
+h = int(input("Height: "))
+a = int(input("First value: "))
+b = int(input("Second value: "))
+
+area = (a + b)/2 * h 
+
+print(f"The area of a trapezoid {area}")
